3e53c691716d75bcc68d17fb61262138435ee108
Tommy X <tommyx058@gmail.com>
2017-11-21 16:12:38 -0500

    add things

---
+++ background.v
@@ -0,0 +1,17 @@
+module draw_background(
+	input start,
+	input clock,
+	output drawing,
+	output x,
+	output y,
+	output colour,	// WE'RE CANADIAN
+	output plot
+	);
+	
+	
+	
+	always @(posedge clock) begin
+		
+	end
+
+endmodule
