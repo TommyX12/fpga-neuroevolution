0e6925363a7e9e730e86570421306bcac496aa11
Tommy X <tommyx058@gmail.com>
2017-11-22 20:21:22 -0500

    ant

--- ant.v
+++ ant.v
@@ -8,6 +8,9 @@ module AntDraw(
     input start,
     output reg finished,
     
+    input [`MEM_ADDR_WIDTH-1:0] x_address,
+    input [`MEM_ADDR_WIDTH-1:0] y_address,
+    
     input finished_dp,
     input [`RESULT_WIDTH-1:0] result_dp,
     output reg start_dp,
@@ -22,6 +25,10 @@ module AntDraw(
     reg delay;
     reg waiting;
     
+    reg first_wait;
+    reg second_wait;
+    reg draw;
+    
     always @(posedge clock) begin
         if (!resetn) begin
             finished <= 1;
@@ -35,6 +42,10 @@ module AntDraw(
             
             delay <= 0;
             waiting <= 0;
+            
+            first_wait = 0;
+            second_wait = 0;
+            draw = 0;
         end
         else begin
             colour <= `COLOUR_WIDTH'b111;
@@ -48,9 +59,10 @@ module AntDraw(
                     plot <= 1;
                     finished <= 0;
                     start_dp <= 1;
-                    instruction_dp <= {4'd1, 9'd0, plot, colour, y, x};
+                    instruction_dp <= {4'd2, 12'd0, x_address};
                     delay <= 1;
                     waiting = 0;
+                    first_wait = 1;
                 end
                 else begin
                     start_dp <= 0;
@@ -67,25 +79,28 @@ module AntDraw(
                 else if (waiting) begin
                     start_dp = 0;
                     if (finished_dp) begin
-                        if (x == `SCREEN_WIDTH - 1) begin
-                            x = `X_COORD_WIDTH'd0;
-                            if (y == `SCREEN_HEIGHT - 1) begin
-                                y = `Y_COORD_WIDTH'd0;
-                                finished <= 1;
-                            end
-                            else begin
-                                y = y + 1;
-                            end
+                        if (first_wait) begin
+                            first_wait = 0;
+                            second_wait = 1;
+                            x = result_dp;
                         end
-                        else begin
-                            x = x + 1;
+                        if (second_wait) begin
+                            second_wait = 0;
+                            draw = 1;
+                            y = result_dp;
+                            start_dp = 1;
+                            instruction_dp <= {4'd2, 12'd0, y_address};
+                            delay = 1;
+                            waiting = 0;
                         end
-                        if (!finished) begin
+                        if (draw) begin
+                            draw = 0;
                             start_dp = 1;
                             instruction_dp = {4'd1, 9'd0, plot, colour, y, x};
                             delay = 1;
                             waiting = 0;
                         end
+                        
                     end
                 end
             end
