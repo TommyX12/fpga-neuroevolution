62f21cc5f6e54dbe6507ccab12f49f1449568851
Tommy X <tommyx058@gmail.com>
2017-11-22 19:27:51 -0500

    fix

--- background.v
+++ background.v
@@ -18,7 +18,7 @@ module DrawBackground(
     reg plot;
 	
     reg delay;
-    reg wait;
+    reg waiting;
     
     always @(posedge clock) begin
         if (!resetn) begin
@@ -30,52 +30,64 @@ module DrawBackground(
             
             start_dp <= 0;
             instruction_dp <= 0;
+            
+            delay <= 0;
+            waiting <= 0;
         end
         else begin
+            colour <= `COLOUR_WIDTH'b010;
+            
             // setup registers on the first cycle
-            if (finished && start) begin
-                x <= `X_COORD_WIDTH'd0;
-                y <= `Y_COORD_WIDTH'd0;
-                
-                colour <= `COLOUR_WIDTH'b000;
-                plot <= 1;
-                finished <= 0;
-                start_dp <= 1;
-                wait = 1;
-            end
-            // update positions as long as we are not finished drawing
-            else if (!finished && finished_dp) begin
-                if (delay) {
-                    start_dp = 1;
-                    wait = 1;
-                }
-                else if (wait) {
-                    start_dp = 0;
-                    if (finished_dp) {
-                        ...
-                        start_dp = 1;
-                        delay = 1;
-                    }
-                }
-				colour <= `COLOUR_WIDTH'b010;
-                if (x == `SCREEN_WIDTH - 1) begin
+            if (finished) begin
+                if (start) begin
                     x <= `X_COORD_WIDTH'd0;
-                    if (y == `SCREEN_HEIGHT - 1) begin
-                        y <= `Y_COORD_WIDTH'd0;
-                        finished <= 1;
-                    end
-                    else begin
-                        y <= y + 1;
+                    y <= `Y_COORD_WIDTH'd0;
+                    
+                    plot <= 1;
+                    finished <= 0;
+                    start_dp <= 1;
+                    instuction_dp <= {4'd1, 9'd0, plot, colour, y, x};
+                    delay <= 1;
+                    waiting = 0;
+                end
+                else begin
+                    start_dp <= 0;
+                    plot <= 0;
+                end
+            end
+            
+            // update positions as long as we are not finished drawing
+            else begin
+                if (delay) begin
+                    start_dp = 1;
+                    waiting = 1;
+                end
+                else if (waiting) begin
+                    start_dp = 0;
+                    if (finished_dp) begin
+                        if (x == `SCREEN_WIDTH - 1) begin
+                            x = `X_COORD_WIDTH'd0;
+                            if (y == `SCREEN_HEIGHT - 1) begin
+                                y = `Y_COORD_WIDTH'd0;
+                                finished <= 1;
+                            end
+                            else begin
+                                y = y + 1;
+                            end
+                        end
+                        else begin
+                            x = x + 1;
+                        end
+                        if (!finished) begin
+                            start_dp = 1;
+                            instuction_dp = {4'd1, 9'd0, plot, colour, y, x};
+                            delay = 1;
+                            waiting = 0;
+                        end
                     end
                 end
-                elss
-            end
-            else if (finished) begin
-                start_dp <= 0;
-                plot <= 0;
             end
         end
-        instuction_dp <= {4'd1, 9'd0, plot, colour, y, x};
     end
 
 endmodule
