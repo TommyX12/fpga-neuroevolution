5f4d0c8ca7f5e40817c2959267fd29b4b4146682
Tommy X <tommyx058@gmail.com>
2017-11-22 19:15:01 -0500

    lepush

--- ant.v
+++ ant.v
@@ -1,2 +1,67 @@
+`include "constants.h"
 
+`define ANT_COLOUR 3'b010;
+
+module AntDraw(
+    input clock,
+    input resetn,
+    input start,
+    output reg finished,
+    
+    input finished_dp,
+    input [`RESULT_WIDTH-1:0] result_dp,
+    output reg start_dp,
+    output reg [`INSTRUCTION_WIDTH-1:0] instruction_dp
+    );
+    
+    reg [`X_COORD_WIDTH-1:0] frag_x;
+	reg [`Y_COORD_WIDTH-1:0] frag_y;
+
+    always @(posedge clock) begin
+        if (!resetn) begin
+            frag_x <= `X_COORD_WIDTH'd0;
+            frag_y <= `Y_COORD_WIDTH'd0;
+
+            x <= `X_COORD_WIDTH'd0;
+            y <= `Y_COORD_WIDTH'd0;
+            colour <= `COLOUR_WIDTH'd0;
+            plot <= 0;
+            finished <= 1;
+        end
+        else begin
+            // setup registers on the first cycle
+            if (finished && start) begin
+                frag_x <= `X_COORD_WIDTH'd0;
+                frag_y <= `Y_COORD_WIDTH'd0;
+                     
+                colour <= `COLOUR_WIDTH'b000;
+                plot <= 1;
+                finished <= 0;
+            end
+            // update positions as long as we are not finished drawing
+            else if (!finished) begin
+                     colour <= `COLOUR_WIDTH'b010;
+                if (frag_x == `SCREEN_WIDTH - 1) begin
+                    frag_x <= `X_COORD_WIDTH'd0;
+                    if (frag_y == `SCREEN_HEIGHT - 1) begin
+                        frag_y <= `Y_COORD_WIDTH'd0;
+                        finished <= 1;
+                    end
+                    else begin
+                        frag_y <= frag_y + 1;
+                    end
+                end
+                else begin
+                    frag_x <= frag_x + 1;
+                end
+            end
+            else if (finished) begin
+                plot <= 0;
+            end
+            x = frag_x;
+            y = frag_y;
+        end
+    end
+
+    endmodule
 
--- background.v
+++ background.v
@@ -1,63 +1,81 @@
 `include "constants.h"
 
 module DrawBackground(
-   input start,
-   input clock,
-   input resetn,
-   output reg [`X_COORD_WIDTH-1:0] x,
-   output reg [`Y_COORD_WIDTH-1:0] y,
-   output reg [`COLOUR_WIDTH-1:0] colour, // WE'RE CANADIAN
-   output reg plot,
-	output reg finished
-   );
+    input start,
+    input clock,
+    input resetn,
+    output reg finished,
+
+    input finished_dp,
+    input [`RESULT_WIDTH-1:0] result_dp,
+    output reg start_dp,
+    output reg [`INSTRUCTION_WIDTH-1:0] instruction_dp
+    );
+    
+    reg [`X_COORD_WIDTH-1:0] x;
+    reg [`Y_COORD_WIDTH-1:0] y;
+    reg [`COLOUR_WIDTH-1:0] colour; // WE'RE CANADIAN
+    reg plot;
 	
-	reg [`X_COORD_WIDTH-1:0] frag_x;
-	reg [`Y_COORD_WIDTH-1:0] frag_y;
-	
+    reg delay;
+    reg wait;
+    
     always @(posedge clock) begin
         if (!resetn) begin
-            frag_x <= `X_COORD_WIDTH'd0;
-            frag_y <= `Y_COORD_WIDTH'd0;
-
+            finished <= 1;
             x <= `X_COORD_WIDTH'd0;
             y <= `Y_COORD_WIDTH'd0;
             colour <= `COLOUR_WIDTH'd0;
             plot <= 0;
-            finished <= 1;
+            
+            start_dp <= 0;
+            instruction_dp <= 0;
         end
         else begin
             // setup registers on the first cycle
             if (finished && start) begin
-                frag_x <= `X_COORD_WIDTH'd0;
-                frag_y <= `Y_COORD_WIDTH'd0;
-					 
-					 colour <= `COLOUR_WIDTH'b000;
-					 plot <= 1;
-					 finished <= 0;
+                x <= `X_COORD_WIDTH'd0;
+                y <= `Y_COORD_WIDTH'd0;
+                
+                colour <= `COLOUR_WIDTH'b000;
+                plot <= 1;
+                finished <= 0;
+                start_dp <= 1;
+                wait = 1;
             end
             // update positions as long as we are not finished drawing
-            else if (!finished) begin
-					 colour <= `COLOUR_WIDTH'b010;
-                if (frag_x == `SCREEN_WIDTH - 1) begin
-                    frag_x <= `X_COORD_WIDTH'd0;
-                    if (frag_y == `SCREEN_HEIGHT - 1) begin
-                        frag_y <= `Y_COORD_WIDTH'd0;
+            else if (!finished && finished_dp) begin
+                if (delay) {
+                    start_dp = 1;
+                    wait = 1;
+                }
+                else if (wait) {
+                    start_dp = 0;
+                    if (finished_dp) {
+                        ...
+                        start_dp = 1;
+                        delay = 1;
+                    }
+                }
+				colour <= `COLOUR_WIDTH'b010;
+                if (x == `SCREEN_WIDTH - 1) begin
+                    x <= `X_COORD_WIDTH'd0;
+                    if (y == `SCREEN_HEIGHT - 1) begin
+                        y <= `Y_COORD_WIDTH'd0;
                         finished <= 1;
                     end
                     else begin
-                        frag_y <= frag_y + 1;
+                        y <= y + 1;
                     end
                 end
-                else begin
-                    frag_x <= frag_x + 1;
-                end
+                elss
             end
             else if (finished) begin
+                start_dp <= 0;
                 plot <= 0;
             end
-            x = frag_x;
-            y = frag_y;
         end
+        instuction_dp <= {4'd1, 9'd0, plot, colour, y, x};
     end
 
 endmodule
